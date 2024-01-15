be6410f5090ca792db11f5c0d7dc29041865b228
mariamik31 <mariam.khutsiashvili@gmail.com>
2024-01-16 02:32:54 +0400

    lsn001

---
+++ script.js
@@ -0,0 +1,41 @@
+//exercise 1
+
+let number = 100;
+
+if(number < 50) 
+{
+    console.log("true")
+} else {
+    console.log("error");
+}
+
+if(number > 20) 
+{
+    console.log("true")
+} 
+
+//exercise 2
+
+let firtsname = "mariami"
+if(firtsname = "mariami") {
+    console.log(true);
+}else if (firtsname = "nika"){
+    console.log("error")
+}
+
+
+//exercise 3
+
+let firstname = "mariami"
+
+switch (firstname) {
+        case "mariami":
+        console.log("true"); 
+        break;
+        case "nino" :
+        console.log("false");
+        break;
+    default:
+        console.log("false");
+         
+}
